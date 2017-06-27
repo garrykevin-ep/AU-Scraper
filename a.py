e4ea44755d7a7aac15a8ee6a6f03eba4cba76e2b
garrykevin <garrykevin.ep@gmail.com>
2017-06-27 21:29:04 +0530

    Multithreading added!!!

--- a.py
+++ a.py
@@ -1,52 +1,80 @@
 from bs4 import BeautifulSoup
-import requests,lxml
+import requests,lxml,random,string
+from datetime import datetime
 import base64
+import threading
 
-url = 'http://coe1.annauniv.edu/home/'
-post_url = 'http://coe1.annauniv.edu/home/students_corner.php'
-s=requests.Session()
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
 
-r = s.get(url)
+def mark():
+	url = 'http://coe1.annauniv.edu/home/'
+	post_url = 'http://coe1.annauniv.edu/home/students_corner.php'
+	reg_no = ''
+	dob =  ''
+
+	s=requests.Session()
+
+	r = s.get(url)
+
+	ti = datetime.now()
+
+	soup = BeautifulSoup(r.content,'lxml')
+	a = soup.find_all('input',type = 'hidden')
+	# print a
+	hidden = a[2]['value']
+	a = soup.find_all('img')
+	image_data = a[8]['src']
+
+	image_name = id_generator()	
+
+	convert = base64.b64decode(image_data[22:])
+
+	t = open(image_name+".png", "wb+")
+	t.write(convert)
+	t.close()
+
+	print ti
+
+	capcha = raw_input('enter capcha '+ image_name + ' : ')
+
+	data = {
+		 hidden : hidden,
+		'register_no': reg_no,
+		'dob': dob,
+		 'security_code_student' : capcha,
+		 'gos' : 'Login',
+	}
 
 
-soup = BeautifulSoup(r.content,'lxml')
-a = soup.find_all('input',type = 'hidden')
-hidden = a[2]['value']
-a = soup.find_all('img')
-image_data = a[8]['src']
+	r = s.post(post_url,data=data)
 
+	# print(r.content)
 
-convert = base64.b64decode(image_data[22:])
+	soup = BeautifulSoup(r.content,'lxml')
+	a = soup.find_all('input',type='hidden')
 
-t = open("capcha.png", "wb+")
-t.write(convert)
-t.close()
+	hidden = a[0]['value']
 
-capcha = raw_input('enter capcha : ')
+	data = {
+		hidden : hidden,
+		'ExamResults' : '',
+		'univ_reg_no' : '',
+	}
 
-data = {
-	 hidden : hidden,
-	'register_no': '',
-	'dob': '',
-	 'security_code_student' : capcha,
-	 'gos' : 'Login',
-}
+	r = s.post(post_url,data = data)
+	
+	out = open("a.txt","w")
+	out.write(r.content)
+	out.close()
 
+	# print r.content
 
-r = s.post(post_url,data=data)
+t1 = threading.Thread(target=mark, args=[])
+t2 = threading.Thread(target=mark, args=[])
+t3 = threading.Thread(target=mark, args=[])
 
-# print(r.content)
-
-soup = BeautifulSoup(r.content,'lxml')
-a = soup.find_all('input',type='hidden')
-
-hidden = a[0]['value']
-
-data = {
-	hidden : hidden,
-	'ExamResults' : '',
-	'univ_reg_no' : '',
-}
-
-r = s.post(post_url,data = data)
-print r.content
+t1.start()
+t2.start()
+t3.start()
+# mark()
